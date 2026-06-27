64b57e3be4f87f590927a70211911da02c9e96fe
Sivangi77 <sivangikashyap9@gmail.com>
2026-06-27 18:17:47 +0530

    Authentication and user route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDb from "./utils/connectDb.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    }
+))
+app.use(express.json())
+app.use(cookieParser())
+
 const PORT = process.env.PORT || 5000;
 
 
@@ -12,6 +27,8 @@ app.get('/', (req, res)=>{
     res.json({message: "Backend running"});
 });
 
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
